471da29735e1552401b47a0f5e052cda6ef0b402
solomon-winner <solyalew3@gmail.com>
2024-07-12 12:35:17 +0300

    ...

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:video_player_app/video_info.dart';
+import 'package:workout_scheduler/video_info.dart';
 import 'colors.dart' as color;
 
 class HomePage extends StatefulWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:video_player_app/video_info.dart';
+import 'package:workout_scheduler/video_info.dart';
 
 import 'home_page.dart';
 
