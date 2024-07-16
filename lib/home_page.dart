99d216d36a398a2918d5a946acceeb703537fe39
solomon-winner <solyalew3@gmail.com>
2024-07-16 15:53:22 +0300

    ...

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -23,7 +23,7 @@ class _HomePageState extends State<HomePage> {
             _buildSubtitleRow(),
             SizedBox(height: 20),
             _buildWorkoutCard(context),
-             SizedBox(height: 20),
+            //  SizedBox(height: 20),
             _buildOverlapingCard(context),
           ],
         ),
@@ -204,16 +204,18 @@ class _HomePageState extends State<HomePage> {
   }
 
   Container _buildOverlapingCard(BuildContext context) {
+    
     return Container(
       width: MediaQuery.of(context).size.width,
       height:120,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         image: DecorationImage(
-          image: AssetImage("assets\card.jpg"),
+        image: AssetImage("assets/card.jpg"),
           ),
         
       ),
+      
     );
   }
 }
