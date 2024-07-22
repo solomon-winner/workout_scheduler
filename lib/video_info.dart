1ff9fd6518002d00f2d87c5bc4c4714235cf84dd
solomon-winner <solyalew3@gmail.com>
2024-07-22 20:15:39 +0300

    ...

--- lib/video_info.dart
+++ lib/video_info.dart
@@ -45,12 +45,12 @@ class _VideoInfoState extends State<VideoInfo> {
                       )
                     ],
                   ),
-
+                  SizedBox(height: 30,),
             Text(
               "Legs Toning",
               style: TextStyle(
                 fontSize: 25,
-                color: color.AppColor.homePageContainerTextSmall,
+                color: color.AppColor.secondPageTitleColor,
               ),
             ),
             SizedBox(height: 5),
@@ -58,10 +58,61 @@ class _VideoInfoState extends State<VideoInfo> {
               "and Glutes workout",
               style: TextStyle(
                 fontSize: 25,
-                color: color.AppColor.homePageContainerTextSmall,
+                color: color.AppColor.secondPageTitleColor,
               ),
             ),
-
+            SizedBox(height: 50),
+            Row(
+              children: [
+                Container(
+                  width: 90,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(colors: [
+                      color.AppColor.secondPageContainerGradient1stColor,
+                      color.AppColor.secondPageContainerGradient2ndColor,
+                    ],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight
+                    )
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.timer,size: 20,
+                      color: color.AppColor.secondPageIconColor,
+                     
+                      ),
+                      SizedBox(width:5),
+                      Text("68 min",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: color.AppColor.secondPageIconColor
+                      ),
+                      )
+                    ],),
+                ),
+                                Container(
+                  width: 90,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(colors: [
+                      color.AppColor.secondPageContainerGradient1stColor,
+                      color.AppColor.secondPageContainerGradient2ndColor,
+                    ],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                    )
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.info_outline)
+                    ],
+                  ),
+                )
+              ],
+            )
                 ],
               )
             )
